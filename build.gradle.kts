231f70fd057f68d754231e178f071a44b0e6326e
MirageFairy <tacticsrealize2@gmail.com>
2025-11-08 21:02:12 +0900

    build: change: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,116 +1,69 @@
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry
-import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
-import java.io.DataInputStream
-import java.io.FileInputStream
-import java.nio.ByteBuffer
-import java.nio.charset.Charset
-import java.util.zip.GZIPInputStream
-
-buildscript {
-    repositories {
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.apache.commons:commons-compress:1.27.1")
-    }
+plugins {
+    java
+    id("build-logic")
 }
 
-fun ByteArray.decodeEucJp() = Charset.forName("EUC-JP").decode(ByteBuffer.wrap(this)).toString()
-fun String.decodeTitle() = this.chunked(2).map { it.toInt(16).toByte() }.toByteArray().decodeEucJp()
+val srcZipFileName = "4aa2d80d-6d2b-48e0-b0aa-a507c4643eb5.tar.gz"
 
-fun File.getZipEntries() = sequence {
-    FileInputStream(this@getZipEntries).use { fis ->
-        GZIPInputStream(fis).use { gis ->
-            TarArchiveInputStream(gis).use { tis ->
-                var entry = tis.nextTarEntry
-                while (entry != null) {
+// Wiki Dump を展開
+val unpackWikiDump = tasks.register<Sync>("unpackWikiDump") {
+    group = "build"
+    from(tarTree(resources.gzip(layout.projectDirectory.dir("dumps").file(srcZipFileName))))
+    into(layout.buildDirectory.dir("unpackWikiDump"))
+}
 
-                    yield(Pair(entry, tis))
-
-                    entry = tis.nextTarEntry
+// テキストに出力
+val generateDumpText = tasks.register("generateDumpText") {
+    group = "build"
+    inputs.files(unpackWikiDump)
+    val destinationFile = layout.projectDirectory.file("all.wiki.txt")
+    outputs.file(destinationFile)
+    doLast {
+        destinationFile.asFile.parentFile?.mkdirs()
+        destinationFile.asFile.printWriter().use { writer ->
+            unpackWikiDump.get().outputs.files.asFileTree
+                .sortedBy { it.path }
+                .asSequence()
+                .readAsWikiPages(this@register)
+                .forEach { (title, body) ->
+                    writer.print("##########################################")
+                    writer.print("\n")
+                    writer.print("##  Page: $title")
+                    writer.print("\n")
+                    writer.print("##########################################")
+                    writer.print("\n")
+                    writer.print("")
+                    writer.print("\n")
+                    writer.print(body)
+                    writer.print("\n")
                 }
-            }
         }
+        logger.lifecycle("Written: ${destinationFile.asFile.absolutePath}")
     }
 }
+tasks.named("assemble").configure { dependsOn(generateDumpText) }
 
-fun Sequence<Pair<TarArchiveEntry, TarArchiveInputStream>>.visitPages(callback: (String, String) -> Unit) {
-    this.forEach { (entry, tis) ->
-        val fileName = entry.name
-        if (entry.isDirectory) {
-            println("Directory: $fileName")
-            return@forEach
-        }
-        println("File: $fileName")
-
-        val matchResult = """^mifai2024/wiki/((?:[0-9A-F]{2})+)\.txt$""".toRegex().matchEntire(fileName)
-        if (matchResult == null) {
-            println("Invalid file: $fileName")
-            return@forEach
-        }
-        val title = matchResult.groups[1]!!.value.decodeTitle()
-
-        if (title.startsWith(":")) return@forEach
-
-        val buffer = ByteArray(entry.size.toInt())
-        DataInputStream(tis).readFully(buffer)
-
-        callback(title, buffer.decodeEucJp())
-    }
-}
-
-fun dumpAllToSingleText(srcZipFile: File, outFile: File) {
-    outFile.printWriter().use { writer ->
-        val contents = mutableMapOf<String, List<String>>()
-        srcZipFile.getZipEntries().visitPages { name, body ->
-            contents[name] = listOf(
-                "##########################################",
-                "##  Page: $name",
-                "##########################################",
-                "",
-                body,
-            )
-        }
-        contents.entries.sortedBy { it.key }.map { it.value }.flatten().forEach {
-            writer.print("$it\n")
-        }
-    }
-}
-
-fun dumpPagesToDirectory(srcZipFile: File, outDir: File) {
-    val remainingFiles = outDir.walk().filter { it.isFile }.map { it.absoluteFile }.toMutableSet()
-    outDir.mkdirs()
-    srcZipFile.getZipEntries().visitPages { name, body ->
-        val outFile = outDir.resolve("$name.wiki.txt")
-        outFile.writeText(body)
-        remainingFiles -= outFile
-    }
-    remainingFiles.forEach {
-        println("Delete: $it")
-        it.delete()
-    }
-}
-
-
-val srcZipFile = File("3e81b3ca-7be8-4d5c-9ddd-d2e34a683adf.tar.gz")
-val outFile = File("wiki-dump_out.txt")
-val outDir = File("wiki")
-
-tasks.register("build")
-
-tasks.register("dump")
-tasks.named("build") { dependsOn(tasks.named("dump")) }
-
-tasks.register("dumpText") {
+// 一旦フォーマットを整形する
+val dumpWikiDirectory = tasks.register("dumpWikiDirectory") {
+    group = "build"
+    inputs.files(unpackWikiDump)
+    val destinationDirectory = layout.buildDirectory.dir("dumpWikiDirectory")
+    outputs.dir(destinationDirectory)
     doLast {
-        dumpAllToSingleText(srcZipFile, outFile)
+        destinationDirectory.get().asFile.deleteRecursively() || throw Exception("Cannot delete directory: ${destinationDirectory.get().asFile.absolutePath}")
+        destinationDirectory.get().asFile.mkdirs() || throw Exception("Cannot create directory: ${destinationDirectory.get().asFile.absolutePath}")
+        unpackWikiDump.get().outputs.files.asFileTree.asSequence().readAsWikiPages(this@register).forEach { (title, body) ->
+            val destinationFile = destinationDirectory.get().asFile.resolve("${title.escapeFileName()}.wiki.txt")
+            if (destinationFile.exists()) throw Exception("File already exists: ${destinationFile.absolutePath}")
+            destinationFile.writeText(body)
+        }
     }
 }
-tasks.named("dump") { dependsOn(tasks.named("dumpText")) }
 
-tasks.register("dumpDir") {
-    doLast {
-        dumpPagesToDirectory(srcZipFile, outDir)
-    }
+// フォルダに出力
+val generateWikiDirectory = tasks.register<Sync>("generateWikiDirectory") {
+    group = "build"
+    from(dumpWikiDirectory)
+    into(layout.projectDirectory.dir("wiki"))
 }
-tasks.named("dump") { dependsOn(tasks.named("dumpDir")) }
+tasks.named("assemble").configure { dependsOn(generateWikiDirectory) }
